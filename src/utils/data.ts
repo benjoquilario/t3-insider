0939a6a22a41908fa19a8ff079dbec3764b306ab
unknown <benjoquilario@gmail.com>
2023-01-15 12:34:42 +0800

    prisma\

---
+++ src/utils/data.ts
@@ -0,0 +1,21 @@
+export const posts = [
+  {
+    name: "Rocky Bhai",
+    message: "Adhira to gyo",
+    userId: "",
+    selectImage: "",
+  },
+  {
+    name: "Rocky Bhai",
+    message:
+      "Voilence Voilence \nVoilence i don't like, \ni Avoid....But Voilence \nlikes me I can't Avoid.",
+    userId: "",
+    selectImage: "",
+  },
+  {
+    name: "Rocky Bhai",
+    message: "Adhira to gyo",
+    userId: "",
+    selectImage: "",
+  },
+];
